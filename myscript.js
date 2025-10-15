cc58ebadc4fb1c39f9c8f27884b8675337f4a717
Theesy0 <161151615+Theesy0@users.noreply.github.com>
2025-10-15 18:52:44 +0700

    Add computer choice function

--- myscript.js
+++ myscript.js
@@ -1,1 +1,15 @@
 console.log("Hello World!")
+
+function getComputerChoice() {
+    let randNum = Math.random();
+
+    if (randNum > 0.6) {
+        return "Rock";
+    } else if (randNum > 0.3) {
+        return "Paper";
+    }else {
+        return "Scissor";
+    }
+}
+
+getComputerChoice();
