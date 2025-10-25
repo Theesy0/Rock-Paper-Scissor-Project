74f0652342d7b27ebe68a35d8b303bdb5c415905
Theesy0 <161151615+Theesy0@users.noreply.github.com>
2025-10-25 10:15:21 +0700

    Add UI and change JS code

--- myscript.js
+++ myscript.js
@@ -1,100 +1,133 @@
-console.log("Hello World!")
+const playerScores = document.querySelector("#playerScore");
+const computerScores = document.querySelector("#computerScore");
+const playerHand = document.querySelector("#playerChoice");
+const computerHand = document.querySelector("#computerChoice");
+const rockbtn = document.querySelector("#rock");
+const paperbtn = document.querySelector("#paper");
+const scissorsbtn = document.querySelector("#scissors");
+const result = document.querySelector("#result");
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    for (let i = 1; i < 6; i++) {
-        function getComputerChoice() {
-            let randNum = Math.random();
+let playerScore = 0;
+let computerScore = 0;
+let win;
 
-            if (randNum > 0.6) {
-                return "rock";
-            } else if (randNum > 0.3) {
-                return "paper";
-            }else {
-                return "scissor";
-            }
-        }
+function getComputerChoice() {
+    let randNum = Math.random();
 
-
-        function getHumanChoice() {
-            let choice = prompt("Round " + i + "! Please enter either Rock, Paper or Scissor.", "");
-            // console.log (choice);
-            return String(choice);
-        }
-
-        getComputerChoice();
-
-        function playRound(humanChoice, computerChoice) {
-            let win
-
-            if (humanChoice === "rock") {
-                switch (computerChoice) {
-                    case "rock":
-                        win = "none";
-                        break;
-                    case "paper":
-                        win = "computer"
-                        break;
-                    case "scissor":
-                        win = "human";
-                        break;
-                }
-            } else if (humanChoice === "paper") {
-                switch (computerChoice) {
-                    case "rock":
-                        win = "human";
-                        break;
-                    case "paper":
-                        win = "none"
-                        break;
-                    case "scissor":
-                        win = "computer";
-                        break;
-                }
-            } else if (humanChoice === "scissor") {
-                switch (computerChoice) {
-                    case "rock":
-                        win = "computer";
-                        break;
-                    case "paper":
-                        win = "human"
-                        break;
-                    case "scissor":
-                        win = "none";
-                        break;
-                }
-            }
-
-            if (win === "human") {
-                console.log ("You win! 1 point for you!");
-                humanScore ++;
-                console.log("Round " + i);
-                console.log("Human score: " + humanScore);
-                console.log("Computer score: " + computerScore);
-            } else if (win === "computer") {
-                console.log ("You lose! 1 point for the computer!");
-                computerScore ++;
-                console.log("Round " + i);
-                console.log("Human score: " + humanScore);
-                console.log("Computer score: " + computerScore);
-            } else if (win === "none") {
-                console.log ("No one win!");
-                console.log("Round " + i);
-                console.log("Human score: " + humanScore);
-                console.log("Computer score: " + computerScore);
-            } 
-        }
-
-        let humanSelection = getHumanChoice().toLocaleLowerCase();
-        computerSelection = getComputerChoice().toLocaleLowerCase();
-
-        console.log("human chose: " + humanSelection);
-        console.log("computer chose: " + computerSelection);
-
-        playRound(humanSelection, computerSelection);
-
+    if (randNum > 0.6) {
+       return "rock";
+    } else if (randNum > 0.3) {
+       return "paper";
+    }else {
+        return "scissors";
     }
 }
 
-playGame();
+function playGame(choice) {
+    const comchoice = getComputerChoice()
+    playerHand.src = ("images/" + choice + ".png");
+    computerHand.src = ("images/" + comchoice + ".png");
+    playRound(choice, comchoice)
+}
+
+rockbtn.addEventListener("click", function() {playGame("rock");});
+paperbtn.addEventListener('click', function() {playGame("paper");});
+scissorsbtn.addEventListener('click', function() {playGame("scissors");});
+
+function playRound(playerChoice, computerChoice) {
+    console.log(playerChoice + " vs " + computerChoice)
+    if (playerChoice === "rock") {
+        switch (computerChoice) {
+            case "rock":
+                win = "none";
+               break;
+            case "paper":
+                win = "computer"
+                break;
+            case "scissors":
+                win = "player";
+                break;
+        }
+    } else if (playerChoice === "paper") {
+        switch (computerChoice) {
+            case "rock":
+                win = "player";
+                break;
+            case "paper":
+                win = "none"
+                break;
+            case "scissors":
+                win = "computer";
+                break;
+        }
+    } else if (playerChoice === "scissors") {
+        switch (computerChoice) {
+            case "rock":
+                win = "computer";
+                break;
+            case "paper":
+                win = "player"
+                break;
+            case "scissors":
+                win = "none";
+                break;
+        }
+    }
+
+    calculateScore(win);
+}
+
+function calculateScore(win) {
+    if (win === "player") {
+        console.log ("You win! 1 point for you!");
+        playerScore ++;
+        console.log("Player score: " + playerScore);
+        console.log("Computer score: " + computerScore);
+        playerScores.textContent = "Player Score: " + playerScore
+        if (playerScore === 5) {
+            congrat("You");
+        }
+    } else if (win === "computer") {
+        console.log ("You lose! 1 point for the computer!");                 
+        computerScore ++;
+        console.log("Player score: " + playerScore);
+        console.log("Computer score: " + computerScore);
+        computerScores.textContent = "Computer score: " + computerScore;
+        if (computerScore === 5) {
+            congrat("Computer");
+        }
+    } else if (win === "none") {
+        console.log ("No one win!");
+        console.log("Player score: " + playerScore);
+        console.log("Computer score: " + computerScore);
+    } 
+
+}
+
+function congrat(who) {
+    result.textContent = "Congrat! " + who + " win! Please restart the game to play! ";
+    const restartbtn = document.createElement("button");
+    restartbtn.textContent = "Restart Game";
+    result.appendChild(restartbtn);
+
+    rockbtn.removeEventListener("click", function() {playGame("rock");});
+    paperbtn.removeEventListener('click', function() {playGame("paper");});
+    scissorsbtn.removeEventListener('click', function() {playGame("scissors");});
+
+   
+    restartbtn.style.fontSize = "20px";
+    restartbtn.style.backgroundColor = "#585481";
+    restartbtn.style.borderRadius = "8px";
+    restartbtn.style.height = "40px";
+    restartbtn.style.color = "#D1BCE3"
+    restartbtn.addEventListener("click", function() {window.location.reload()})
+}
+
+    //     let humanSelection = getHumanChoice().toLocaleLowerCase();
+    //     computerSelection = getComputerChoice().toLocaleLowerCase();
+
+    //    console.log("human chose: " + humanSelection);
+    //     console.log("computer chose: " + computerSelection);
+
+    //     playRound(humanSelection, computerSelection);
+
